f5dff198e3c3fcc8545e702f1ba8dc4393fadaa0
Early Charlemagne <dev@earlycharlemagne.com>
2023-05-13 01:29:53 +0200

    moved methods to service class

--- src/main/java/com/earlycharlemagne/customerapi/controller/CustomerController.java
+++ src/main/java/com/earlycharlemagne/customerapi/controller/CustomerController.java
@@ -1,7 +1,6 @@
 package com.earlycharlemagne.customerapi.controller;
 
 import java.util.List;
-import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.earlycharlemagne.customerapi.dto.CustomerDto;
-import com.earlycharlemagne.customerapi.entity.Customer;
-import com.earlycharlemagne.customerapi.exception.CustomerCreationException;
-import com.earlycharlemagne.customerapi.exception.CustomerNotFoundException;
-import com.earlycharlemagne.customerapi.repository.CustomerRepository;
+import com.earlycharlemagne.customerapi.service.CustomerService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,93 +22,40 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/customers")
 @RequiredArgsConstructor
 public class CustomerController {
-    private final CustomerRepository repository;
+    private final CustomerService customerService;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<CustomerDto> getCustomers(@RequestParam(required = false) String firstName, @RequestParam(required = false) String lastName) {
         if (firstName != null) {
             if (lastName != null) {
-                return repository.findByFirstNameAndLastName(firstName, lastName)
-                                 .stream()
-                                 .map(this::mapToCustomerDto)
-                                 .toList();
+                return customerService.getCustomerByFirstAndLastName(firstName, lastName);
             }
-            return repository.findByFirstName(firstName)
-                             .stream()
-                             .map(this::mapToCustomerDto)
-                             .toList();
+            return customerService.getCustomerByFirstName(firstName);
         }
 
         if (lastName != null) {
-            return repository.findByLastName(lastName)
-                             .stream()
-                             .map(this::mapToCustomerDto)
-                             .toList();
+            return customerService.getCustomerByLastName(lastName);
         }
 
-        return repository.findAll()
-                         .stream()
-                         .map(this::mapToCustomerDto)
-                         .toList();
+        return customerService.getAllCustomers();
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public CustomerDto getCustomerById(@PathVariable("id") String globalId) {
-        return repository.findByGlobalId(globalId)
-                         .map(this::mapToCustomerDto)
-                         .orElseThrow(() -> new CustomerNotFoundException("Customer with globalId [%s] does not exist".formatted(globalId)));
-    }
-
-    private CustomerDto mapToCustomerDto(Customer customer) {
-        return CustomerDto.builder()
-                          .id(customer.getGlobalId())
-                          .firstName(customer.getFirstName())
-                          .lastName(customer.getLastName())
-                          .age(customer.getAge())
-                          .email(customer.getEmail())
-                          .address(customer.getAddress())
-                          .build();
+        return customerService.getCustomerByGlobalId(globalId);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public String createCustomer(@RequestBody CustomerDto customerDto) {
-        if (customerExists(customerDto)) {
-            throw new CustomerCreationException("Failed to create customer. Email already exists");
-        }
-
-        Customer customer = mapToCustomer(customerDto);
-        customer.setGlobalId(UUID.randomUUID().toString());
-
-        return repository.save(customer).getGlobalId();
+        return customerService.createNewCustomer(customerDto);
     }
 
     @PutMapping("/{id}/address")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void updateCustomerAddress(@PathVariable("id") String globalId, @RequestBody String address) {
-        Customer customer = repository.findByGlobalId(globalId)
-                                      .orElseThrow(() -> new CustomerNotFoundException("Failed to update address. Customer with globalId [%s] does not exist".formatted(globalId)));
-
-        customer.setAddress(address);
-
-        repository.save(customer);
-    }
-
-    private boolean customerExists(CustomerDto customerDto) {
-        return repository.existsByEmail(customerDto.getEmail());
-    }
-
-    private Customer mapToCustomer(CustomerDto customerDto) {
-        Customer customer = new Customer();
-
-        customer.setFirstName(customerDto.getFirstName());
-        customer.setLastName(customerDto.getLastName());
-        customer.setAge(customerDto.getAge());
-        customer.setEmail(customerDto.getEmail());
-        customer.setAddress(customerDto.getAddress());
-
-        return customer;
+        customerService.updateExistingCustomerAddress(globalId, address);
     }
 }
---
+++ src/main/java/com/earlycharlemagne/customerapi/service/CustomerService.java
@@ -0,0 +1,102 @@
+package com.earlycharlemagne.customerapi.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.earlycharlemagne.customerapi.dto.CustomerDto;
+import com.earlycharlemagne.customerapi.entity.Customer;
+import com.earlycharlemagne.customerapi.exception.CustomerCreationException;
+import com.earlycharlemagne.customerapi.exception.CustomerNotFoundException;
+import com.earlycharlemagne.customerapi.repository.CustomerRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+    private final CustomerRepository repository;
+    
+    public List<CustomerDto> getAllCustomers() {
+        return repository.findAll()
+                         .stream()
+                         .map(this::mapToCustomerDto)
+                         .toList();
+    }
+
+    public List<CustomerDto> getCustomerByLastName(String lastName) {
+        return repository.findByLastName(lastName)
+                         .stream()
+                         .map(this::mapToCustomerDto)
+                         .toList();
+    }
+
+    public List<CustomerDto> getCustomerByFirstName(String firstName) {
+        return repository.findByFirstName(firstName)
+                         .stream()
+                         .map(this::mapToCustomerDto)
+                         .toList();
+    }
+
+    public List<CustomerDto> getCustomerByFirstAndLastName(String firstName, String lastName) {
+        return repository.findByFirstNameAndLastName(firstName, lastName)
+                         .stream()
+                         .map(this::mapToCustomerDto)
+                         .toList();
+    }
+
+    public CustomerDto getCustomerByGlobalId(String globalId) {
+        return repository.findByGlobalId(globalId)
+                         .map(this::mapToCustomerDto)
+                         .orElseThrow(() -> new CustomerNotFoundException("Customer with globalId [%s] does not exist".formatted(globalId)));
+    }
+
+    public CustomerDto mapToCustomerDto(Customer customer) {
+        return CustomerDto.builder()
+                          .id(customer.getGlobalId())
+                          .firstName(customer.getFirstName())
+                          .lastName(customer.getLastName())
+                          .age(customer.getAge())
+                          .email(customer.getEmail())
+                          .address(customer.getAddress())
+                          .build();
+    }
+
+    public String createNewCustomer(CustomerDto customerDto) {
+        if (customerExists(customerDto)) {
+            throw new CustomerCreationException("Failed to create customer. Email already exists");
+        }
+
+        Customer customer = mapToCustomer(customerDto);
+        customer.setGlobalId(UUID.randomUUID().toString());
+
+        return repository.save(customer)
+                         .getGlobalId();
+    }
+
+    private boolean customerExists(CustomerDto customerDto) {
+        return repository.existsByEmail(customerDto.getEmail());
+    }
+
+    private Customer mapToCustomer(CustomerDto customerDto) {
+        Customer customer = new Customer();
+
+        customer.setFirstName(customerDto.getFirstName());
+        customer.setLastName(customerDto.getLastName());
+        customer.setAge(customerDto.getAge());
+        customer.setEmail(customerDto.getEmail());
+        customer.setAddress(customerDto.getAddress());
+
+        return customer;
+    }
+
+    public void updateExistingCustomerAddress(String globalId, String address) {
+        Customer customer = repository.findByGlobalId(globalId)
+                                      .orElseThrow(() -> new CustomerNotFoundException("Failed to update address. Customer with globalId [%s] does not exist".formatted(globalId)));
+
+        customer.setAddress(address);
+
+        repository.save(customer);
+    }
+}
